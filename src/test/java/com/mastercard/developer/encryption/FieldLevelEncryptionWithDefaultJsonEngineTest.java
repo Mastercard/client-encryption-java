6885ee6884752cef80c8e34ec6800005cf5f7c58
Aufauvre, Jean-Alexis <jean-alexis.aufauvre@outlook.com>
2019-04-22 10:11:55 +0100

    Added new interoperability test

--- src/test/java/com/mastercard/developer/encryption/FieldLevelEncryptionWithDefaultJsonEngineTest.java
+++ src/test/java/com/mastercard/developer/encryption/FieldLevelEncryptionWithDefaultJsonEngineTest.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.mastercard.developer.test.TestUtils;
+import com.mastercard.developer.utils.EncryptionUtils;
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
@@ -63,6 +64,29 @@ public class FieldLevelEncryptionWithDefaultJsonEngineTest {
         // THEN
         byte[] expectedBytes = "some data ù€@".getBytes();
         Assert.assertArrayEquals(expectedBytes, decryptedBytes);
+    }
+
+    @Test
+    public void testDecryptPayload_InteroperabilityTest() throws Exception {
+
+        // GIVEN
+        String encryptedPayload = "{\"data\":\"WtBPYHL5jdU/BsECYzlyRUPIElWCwSCgKhk5RPy2AMZBGmC8OUJ1L9HC/SF2QpCU+ucZTmo7XOjhSdVi0/yrdZP1OG7dVWcW4MEWpxiU1gl0fS0LKKPOFjEymSP5f5otdTFCp00xPfzp+l6K3S3kZTAuSG1gh6TaRL+qfC1POz8KxhCEL8D1MDvxnlmchPx/hEyAzav0AID3T7T4WomzUXErNrnbDCCiL6pm4IBR8cDAzU4eSmTxdzZFyvTpBQDXVyFdkaNTo3GXk837wujVK8EX3c+gsJvMq4XVJFwGmPNhPM6P7OmdK45cldWrD5j2gO2VBH5aW1EXfot7d11bjJC9T8D/ZOQFF6uLIG7J9x9R0Ts0zXD/H24y9/jF30rKKX7TNmKHn5uh1Czd+h7ryIAqaQsOu6ILBKfH7W/NIR5qYN1GiL/kOYwx2pdIGQdcdolVdxV8Z6bt4Tcvq3jSZaCbhJI/kphZL7QHJgcG6luz9k0457x/0QCDPlve6JNgUQzAOYC64X0a07JpERH0O08/YbntKEq6qf7UhloyI5A=\"}";
+        FieldLevelEncryptionConfig config = getTestFieldLevelEncryptionConfigBuilder()
+                .withDecryptionKey(EncryptionUtils.loadDecryptionKey("./src/test/resources/keys/pkcs1/test_key_pkcs1-2048.pem"))
+                .withDecryptionPath("$", "$")
+                .withEncryptedValueFieldName("data")
+                .withFieldValueEncoding(FieldValueEncoding.BASE64)
+                .build();
+        String oaepPaddingDigest = "SHA256";
+        String encryptedKey = "dobCRy+NUxdQdN0oMLT4dXUzQ+We7BahMfJunoAmwwUpk9jJrW66BASPalS2QWChPaKDM4Ft/BeNsu0wBoUZ0hHIT9ftx5g4tY6Xu2iLRiFWFDCHYOSdL+yVv98FcM6fxc34FNyg3/rOPWeyS3Q9YAOgcqiCwWYu4kqa34tNWCW1vnTmtz+dCKiiCZo/uHUkCtoAI5fEe+inHHToZL+LFlQ2Xd0u/nsu5Ep14Il5mTv8FyfLgwRgfilcqy4t2Kh3bpZ46LllO36DHXtQoI1e0ayMFfKTO87++NWxYNOilrverJ01WHnA+PyXhg4XU3RlU0CVWBN06fKbHBDH6GCmOA==";
+        String iv = "+yBXlo+gYGe2q0xzLDLLzQ==";
+
+        // WHEN
+        FieldLevelEncryptionParams params = new FieldLevelEncryptionParams(iv, encryptedKey, oaepPaddingDigest, config);
+        String payload = FieldLevelEncryption.decryptPayload(encryptedPayload, config, params);
+
+        // THEN
+        Assert.assertTrue(payload.contains("account"));
     }
 
     @Test
@@ -1165,10 +1189,7 @@ public class FieldLevelEncryptionWithDefaultJsonEngineTest {
         String ivValue = "ba574b07248f63756bce778f8a115819";
         String encryptedKeyValue = "26687f6d03d27145451d20bdaa29cc199e2533bb9eb7351772e31d1290b98380b43dbf47b9a337cc2ecaff9d3d9fb45305950f13382c5ad822ee6df79e1a57b14a3c58c71090121994a9f771ef96472669671718b55a0fa8d9f76de9e172fedcabbc87d64b5a994899e43abb19afa840269012c397b5b18d4babc0e41c1ad698db98c89121bbe5b2d227cfc5d3c3c87f4f4c8b04b509d326199b39adfbd8bca8bf0a150fcf3c37b9717382af502ad8d4d28b17b91762bf108d34aba0fb40ca410c2ecaeb30d68003af20dce27d9d034e4c557b8104e85f859de0eb709b23f9978869bae545c7f1b62173887eae9e75e4b6d6b4b01d7172ccc8c5774c0db51c24";
         String oaepHashingAlgorithmValue = "SHA256";
-        String encryptionCertificateFingerprintValue = "80810fc13a8319fcf0e2ec322c82a4c304b782cc3ce671176343cfe8160c2279";
-        String encryptionKeyFingerprintValue = "761b003c1eade3a5490e5000d37887baa5e6ec0e226c07706e599451fc032a79";
-        FieldLevelEncryptionParams params = new FieldLevelEncryptionParams(ivValue, encryptedKeyValue, oaepHashingAlgorithmValue,
-                                                                           config);
+        FieldLevelEncryptionParams params = new FieldLevelEncryptionParams(ivValue, encryptedKeyValue, oaepHashingAlgorithmValue, config);
 
         // WHEN
         String payload = FieldLevelEncryption.decryptPayload(encryptedPayload, config, params);
