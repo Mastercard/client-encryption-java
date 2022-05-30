8bbfe78417a456c17dae6809c6a8949489021a6f
Aufauvre, Jean-Alexis <jean-alexis.aufauvre@outlook.com>
2022-05-30 08:59:12 +0100

    Added JweEncryption tests

--- src/test/java/com/mastercard/developer/encryption/JweEncryptionWithDefaultJsonEngineTest.java
+++ src/test/java/com/mastercard/developer/encryption/JweEncryptionWithDefaultJsonEngineTest.java
@@ -3,9 +3,11 @@ package com.mastercard.developer.encryption;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 
 import static com.mastercard.developer.test.TestUtils.*;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class JweEncryptionWithDefaultJsonEngineTest {
 
@@ -32,6 +34,24 @@ public class JweEncryptionWithDefaultJsonEngineTest {
     }
 
     @Test
+    public void testEncryptPayload_ShouldCreateEncryptedValue_WhenOutPathParentDoesNotExistInPayload() throws Exception {
+
+        // GIVEN
+        String payload = "{\"data\": {}}";
+        JweConfig config = getTestJweConfigBuilder()
+                .withEncryptionPath("$", "$.encryptedDataParent")
+                .build();
+
+        // WHEN
+        String encryptedPayload = JweEncryption.encryptPayload(payload, config);
+
+        // THEN
+        JsonObject encryptedPayloadObject = new Gson().fromJson(encryptedPayload, JsonObject.class);
+        assertNull(encryptedPayloadObject.get("data"));
+        Assertions.assertNotNull(encryptedPayloadObject.get("encryptedDataParent").getAsJsonObject().get("encryptedData"));
+    }
+
+    @Test
     public void testDecryptPayload_ShouldDecryptRootArrays() throws Exception {
 
         // GIVEN
@@ -47,5 +67,25 @@ public class JweEncryptionWithDefaultJsonEngineTest {
 
         // THEN
         assertPayloadEquals("[{},{}]", payload);
+    }
+
+    @Test
+    public void testDecryptPayload_ShouldSupportPayloadWithEncryptedValueParent() throws Exception {
+
+        // GIVEN
+        String encryptedPayload = "{\n" +
+                "    \"encryptedDataParent\": {\n" +
+                "        \"encryptedData\": \"eyJraWQiOiI3NjFiMDAzYzFlYWRlM2E1NDkwZTUwMDBkMzc4ODdiYWE1ZTZlYzBlMjI2YzA3NzA2ZTU5OTQ1MWZjMDMyYTc5IiwiY3R5IjoiYXBwbGljYXRpb24vanNvbiIsImVuYyI6IkEyNTZHQ00iLCJhbGciOiJSU0EtT0FFUC0yNTYifQ.XVy1AR51sUvwT-AtcsogQDo_klFi1EMYW8Wz7qM0e1dA3jNX5nTa38JhRcVuyVK15OenTYfg7aaH_fLjPZI1Mukd0BBnTuonh8T9CX5tbAAYx_KGPxc7a7ekBO-xXEA762eRvIIQJDZgQ_C3U39kc-XoaxC-ZYx8Va_aPBsXI1uozAfj3j5XVDnSmGAVWc2N4STTlCKbL4EO6YXASl_PrAOIVVSUrhpYvNS7GnjrP9x49tlRmTS0Dx-_MhkIAJM6H25YAuUmO-LW3gikReOUgGeY9_JtOioDs2J4ncKqugPFKr8kYF1cKnMwFv0TS9p5qR0kiF20bxRMvhbazf_Q5Q.V2Uz5-YRNq9ZIJjhRsKYIw.jB1s8rczGEj2OjU.qs4zVUf2tHML02Rglq5ncw\"\n" +
+                "    }\n" +
+                "}";
+        JweConfig config = getTestJweConfigBuilder()
+                .withDecryptionPath("$.encryptedDataParent.encryptedData", "$")
+                .build();
+
+        // WHEN
+        String payload = JweEncryption.decryptPayload(encryptedPayload, config);
+
+        // THEN
+        assertPayloadEquals("{\"data\": {}}", payload);
     }
 }
