b55f020737c2e60ce78053bd10565c6b183f35a8
tomcrofts <127945242+tomcrofts@users.noreply.github.com>
2024-06-06 14:53:28 +0100

    Update AESCBC.java
    Fixing error, removing IV

--- src/main/java/com/mastercard/developer/encryption/aes/AESCBC.java
+++ src/main/java/com/mastercard/developer/encryption/aes/AESCBC.java
@@ -4,7 +4,6 @@ import com.mastercard.developer.encryption.jwe.JweObject;
 import com.mastercard.developer.utils.EncodingUtils;
 
 import javax.crypto.Cipher;
-import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.GCMParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.security.GeneralSecurityException;
@@ -24,14 +23,13 @@ public class AESCBC {
         SecretKeySpec aesKey = new SecretKeySpec(secretKey.getEncoded(), 16, 16, "AES");
         GCMParameterSpec gcmSpec = new GCMParameterSpec(128, EncodingUtils.base64Decode(object.getIv()));
         byte[] cipherText = EncodingUtils.base64Decode(object.getCipherText());
-        byte[] iv = EncodingUtils.base64Decode(object.getIv());
 
-        return cipher(aesKey, gcmSpec, new IvParameterSpec(iv), cipherText, Cipher.DECRYPT_MODE);
+        return cipher(aesKey, gcmSpec, cipherText, Cipher.DECRYPT_MODE);
     }
 
-    public static byte[] cipher(Key key,GCMParameterSpec gcpSpec, AlgorithmParameterSpec iv, byte[] bytes, int mode) throws GeneralSecurityException {
+    public static byte[] cipher(Key key,GCMParameterSpec gcpSpec, byte[] bytes, int mode) throws GeneralSecurityException {
         Cipher cipher = Cipher.getInstance(CYPHER);
-        cipher.init(mode, key, gcpSpec, iv);
+        cipher.init(mode, key, gcpSpec);
         return cipher.doFinal(bytes);
     }
 }
