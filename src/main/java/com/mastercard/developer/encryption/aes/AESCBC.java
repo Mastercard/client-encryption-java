ad4cb121b3b1ac4690e628060765af4d6103d07e
tomcrofts <127945242+tomcrofts@users.noreply.github.com>
2024-06-06 14:47:03 +0100

    Update AESCBC.java
    Adding import statement

--- src/main/java/com/mastercard/developer/encryption/aes/AESCBC.java
+++ src/main/java/com/mastercard/developer/encryption/aes/AESCBC.java
@@ -5,6 +5,7 @@ import com.mastercard.developer.utils.EncodingUtils;
 
 import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.GCMParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.security.GeneralSecurityException;
 import java.security.Key;
