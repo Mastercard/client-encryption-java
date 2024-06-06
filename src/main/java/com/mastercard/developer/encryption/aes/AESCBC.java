06ac88d9988f2cdaada6d5ab81a8f30c6c2d8a7f
tomcrofts <127945242+tomcrofts@users.noreply.github.com>
2024-06-06 15:36:01 +0100

    Update AESCBC.java
    reverting

--- src/main/java/com/mastercard/developer/encryption/aes/AESCBC.java
+++ src/main/java/com/mastercard/developer/encryption/aes/AESCBC.java
@@ -1,15 +1,14 @@
 package com.mastercard.developer.encryption.aes;
 
 import com.mastercard.developer.encryption.jwe.JweObject;
+import com.mastercard.developer.utils.ByteUtils;
 import com.mastercard.developer.utils.EncodingUtils;
 
 import javax.crypto.Cipher;
 import javax.crypto.spec.GCMParameterSpec;
-import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
 import java.security.GeneralSecurityException;
 import java.security.Key;
-import java.security.spec.AlgorithmParameterSpec;
 
 public class AESCBC {
 
@@ -20,17 +19,16 @@ public class AESCBC {
 
     @java.lang.SuppressWarnings("squid:S3329")
     public static byte[] decrypt(Key secretKey, JweObject object) throws GeneralSecurityException {
-        // First 16 bytes are the MAC key, so we only use the second 16 bytes
-        SecretKeySpec aesKey = new SecretKeySpec(secretKey.getEncoded(), 16, 16, "AES");
+        byte[] aad = object.getRawHeader().getBytes(StandardCharsets.US_ASCII);
         GCMParameterSpec gcmSpec = new GCMParameterSpec(128, EncodingUtils.base64Decode(object.getIv()));
-        byte[] cipherText = EncodingUtils.base64Decode(object.getCipherText());
         byte[] bytes = ByteUtils.concat(EncodingUtils.base64Decode(object.getCipherText()), EncodingUtils.base64Decode(object.getAuthTag()));
 
-        return cipher(aesKey, gcmSpec, bytes, aad, cipherText, Cipher.DECRYPT_MODE);
+        return cipher(cek, gcmSpec, bytes, aad, Cipher.DECRYPT_MODE);
     }
 
-    public static byte[] cipher(Key key,GCMParameterSpec gcpSpec, byte[] bytes, int mode) throws GeneralSecurityException {
+    public static byte[] cipher(Key key, GCMParameterSpec gcpSpec, byte[] bytes, aad, int mode) throws GeneralSecurityException {
         Cipher cipher = Cipher.getInstance(CYPHER);
+        cipher.init(mode, key, gcpSpec);
         cipher.updateAAD(aad);
         return cipher.doFinal(bytes);
     }
